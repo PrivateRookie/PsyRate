949a86e9a298bb1af6fbdfee9ee53dc785999ce9
PrivateRookie <996514515@qq.com>
2017-11-14 22:36:11 +0800

    Project start

---
+++ config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = os.environ.get('SMTP_SERVER')
+    MAIL_PORT = os.environ.get('SMTP_PORT')
+    MAIL_USE_SSL = os.environ.get('USE_SSL')
+    MIAL_USE_TLS = os.environ.get('USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') \
+    or ('sqlite:///' + os.path.join(basedir, 'data-dev.sqlite'))
+
+config = {'default':DevelopmentConfig,
+          'dev':DevelopmentConfig}
