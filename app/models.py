d96cb1dadae7e80ec9e8a4c9b915ced701488b28
PrivateRookie <996514515@qq.com>
2017-11-19 18:32:35 +0800

    添加了用户，患者，和QIDS表，用户测试表关系

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(64))
+    patients = db.relationship('Patient', backref='recorder_lookup')
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -16,3 +17,51 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+    
+class Patient(db.Model):
+    __tablename__ = 'patient_infos'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(32), unique=True)
+    name = db.Column(db.String(32))
+    entry_date = db.Column(db.Date)
+    doctor = db.Column(db.String(32))
+    recorder = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return '<Patient {}>'.format(self.name)   
+    
+class FollowUp(db.Model):
+    __tablename__ = 'follow_ups'
+    id = db.Column(db.Integer, primary_key=True)
+    p_id = db.Column(db.Integer, db.ForeignKey('patient_infos.id'))
+    status = db.Column(db.String(16))
+    follow_up_date = db.Column(db.Date)
+    follow_up_way = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return '<FollowUP {}>'.format(self.status)
+    
+class QIDS(db.Model):
+    __tablename__ = 'qids_sr16'
+    id = db.Column(db.Integer, primary_key=True)
+    p_id = db.Column(db.Integer, db.ForeignKey('patient_infos.id'))
+    status = db.Column(db.String(16), db.ForeignKey('follow_ups.status'))
+    q_1 = db.Column(db.Integer)
+    q_2 = db.Column(db.Integer)
+    q_3 = db.Column(db.Integer)
+    q_4 = db.Column(db.Integer)
+    q_5 = db.Column(db.Integer)
+    q_6 = db.Column(db.Integer)
+    q_7 = db.Column(db.Integer)
+    q_8 = db.Column(db.Integer)
+    q_9 = db.Column(db.Integer)
+    q_10 = db.Column(db.Integer)
+    q_11 = db.Column(db.Integer)
+    q_12 = db.Column(db.Integer)
+    q_13 = db.Column(db.Integer)
+    q_14 = db.Column(db.Integer)
+    q_15 = db.Column(db.Integer)
+    q_16 = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return '<QIDS {}>'.format(self.id)
