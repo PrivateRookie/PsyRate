308e3123c68355496be394a319cb304c40db3f61
PrivateRookie <996514515@qq.com>
2017-11-21 22:54:06 +0800

    网站基本功能完成
    完成功能
    - 用户登陆与注册
    - 表单模板宏完成
    - 用户、患者、和随访及基本表单模型建立完成
    - 模型测试用单元测试完成
    待完成
    -[] 用户身份功能
    -[] 获取从网页返回的表单信息，并在echo上显示方便调试
    -[] 完成所有表单的转换
    -[] 实现所有表单关系
    -[] 网站美化

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, redirect, url_for, flash, session
+from flask import render_template, redirect, url_for, flash, session, request
 from . import main
 from .forms import *
 from .. import db
+from ..static import raw_forms
 
-@main.route('/index', methods=['GET', 'POST'])
+@main.route('/', methods=['GET', 'POST'])
 def index():
     form = DemoForm()
     if form.validate_on_submit():
         session['msg'] = form.msg.data
         return redirect(url_for('.index'))
     return render_template('index.html', form=form, msg=session.get('msg'))
+
+@main.route('/forms')
+def forms():
+    form_name = request.args.get('form_name', None)
+    if not form_name:
+        return render_template('allforms.html')
+    raw_form = getattr(raw_forms, form_name, None)
+    return render_template('rates/' + form_name + '.html', survey=raw_form, route='main.echo')
+    
+    
+@main.route('/echo', methods=['GET', 'POST'])
+def echo():
+    data = [(attr, request.form.get(attr)) for attr in request.form.keys() if attr.startswith('q')]
+    return render_template('echo.html', data=data)
--- app/models.py
+++ app/models.py
@@ -65,3 +65,35 @@ class QIDS(db.Model):
     
     def __repr__(self):
         return '<QIDS {}>'.format(self.id)
+        
+class DSSS(db.Model):
+    __tablename__ = 'dsss'
+    id = db.Column(db.Integer, primary_key=True)
+    p_id = db.Column(db.Integer, db.ForeignKey('patient_infos.id'))
+    status = db.Column(db.String(16), db.ForeignKey('follow_ups.status'))
+    q_1 = db.Column(db.Integer)
+    q_2 = db.Column(db.Integer)
+    q_3 = db.Column(db.Integer)
+    q_4 = db.Column(db.Integer)
+    q_5 = db.Column(db.Integer)
+    q_6 = db.Column(db.Integer)
+    q_7 = db.Column(db.Integer)
+    q_8 = db.Column(db.Integer)
+    q_9 = db.Column(db.Integer)
+    q_10 = db.Column(db.Integer)
+    q_11 = db.Column(db.Integer)
+    q_12 = db.Column(db.Integer)
+    q_13 = db.Column(db.Integer)
+    q_14 = db.Column(db.Integer)
+    q_15 = db.Column(db.Integer)
+    q_16 = db.Column(db.Integer)
+    q_17 = db.Column(db.Integer)
+    q_18 = db.Column(db.Integer)
+    q_19 = db.Column(db.Integer)
+    q_20 = db.Column(db.Integer)
+    q_21 = db.Column(db.Integer)
+    q_22 = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return '<DSSS {}>'.format(self.id)
+ 
