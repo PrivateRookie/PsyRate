1ca1d0c9121244086f15445e2d62a9498062958d
PrivateRookie <996514515@qq.com>
2017-11-21 22:49:56 +0800

    用于模型测试的单元测试

---
+++ tests/test_db.py
@@ -0,0 +1,52 @@
+import unittest
+from datetime import date as Date
+from random import randint
+from flask import current_app
+from app import create_app, db
+from app.models import User, Patient, FollowUp
+
+class VirtualFillTestCase(unittest.TestCase):
+    def setUp(self):
+        # initializ app
+        #self.app = create_app('test')
+        #self.app_context = self.app.app_context()
+        #self.app_context.push()
+        # initializ database
+        db.drop_all()
+        db.create_all()
+        # create test user
+        self.test_user = User(username="Test_User", email="test@test.com",
+            password='PsyRatesTest')
+        db.session.add(self.test_user)
+        db.session.commit()
+        # create test patient
+        self.test_patient = Patient(code='TEST001', name='Test',
+            entry_date=Date(2017,11,21), doctor='Test Doctor',
+            recorder=self.test_user.id)
+        db.session.add(self.test_patient)
+        db.session.commit()
+        # create follow_ups
+        follow_ups = [FollowUp(p_id=self.test_patient.id,
+            status='v{}'.format(i), follow_up_date=Date(2017, 11, i),
+            follow_up_way=2) for i in range(1, 9)]
+        db.session.add_all(follow_ups)
+        db.session.commit()       
+    
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        #self.app_context.pop()
+        
+    def test_dsss(self):
+        from app.models import DSSS
+        data = {'q_{}'.format(i):randint(0, 4) for i in range(1, 23)}
+        data.update(p_id=self.test_patient.id, status='v2')
+        db.session.add(DSSS(**data))
+        self.assertTrue(db.session.commit() is None)
+        
+    def test_qids(self):
+        from app.models import QIDS
+        data = {'q_{}'.format(i):randint(0, 4) for i in range(1, 17)}
+        data.update(p_id=self.test_patient.id, status='v2')
+        db.session.add(QIDS(**data))
+        db.session.commit()
