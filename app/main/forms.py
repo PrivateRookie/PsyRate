6b37345d4aa83f8976f78b061f8dd743372b534d
PrivateRookie <996514515@qq.com>
2017-11-28 22:00:59 +0800

    删除了无用的FlaskForm’

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,3 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
-class DemoForm(FlaskForm):
-    msg = StringField('给自己写段消息吧', validators=[Required()])
-    submit = SubmitField('提交')
