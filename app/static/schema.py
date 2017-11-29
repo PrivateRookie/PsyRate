98b23b4653ad33eb6ecd8930925f6d5051d004de
PrivateRookie <996514515@qq.com>
2017-11-29 20:12:37 +0800

    表格排列纲要

---
+++ app/static/schema.py
@@ -0,0 +1,9 @@
+self_report_schema = \
+{'v1': [, ],
+ 'v2': ['qids', 'dsss', 'pain', 'mfi_20', 'psqi', 'meq_sa', 'staxi_2', 'ipas', 'mdq', 'les', 'ssp', 'pbi', 'ctq'],
+ 'v3': ['dsss', 'mfi_20'],
+ 'v4': ['qids', 'dsss', 'pain', 'mfi_20', 'psqi', 'meq_sa', 'staxi_2'],
+ 'v5': ['qids', 'dsss', 'pain', 'mfi_20', 'psqi', 'meq_sa', 'staxi_2'],
+ 'v6': ['qids', 'dsss', 'pain', 'mfi_20', 'psqi', 'meq_sa', 'staxi_2'],
+ 'v7': ['qids', 'dsss', 'pain', 'mfi_20', 'psqi', 'meq_sa', 'staxi_2'],
+ 'v8': ['qids', 'dsss', 'pain', 'mfi_20', 'psqi', 'meq_sa', 'staxi_2']}
