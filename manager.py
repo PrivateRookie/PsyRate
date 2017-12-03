fe0a781855f67c2c11e95a7e1f7843d9c6d88e0e
PrivateRookie <996514515@qq.com>
2017-12-03 22:04:42 +0800

    丰富了命令行功能
    -
    添加了init_db命令，现在可以在命令行下直接重置数据库，并且Role和管理员角色都已准备就绪
    - 增加了test命令的功能，新增参数-n --name 用于指定测试的表名

--- manager.py
+++ manager.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
 from app import create_app, db
-from flask_script import Manager, Shell
+from app.models import Role, User
+from flask_script import Manager, Shell, Command, Option
+from tests.test_db import VirtualFillTestCase
 
 from app import models
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'test')
 manager = Manager(app)
 
 def make_shell_context():
@@ -13,11 +15,28 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 @manager.command
-def test():
+def test(name=None):
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    if name is None:
+        tests = unittest.TestLoader().discover('tests')
+        unittest.TextTestRunner(verbosity=2).run(tests)
+    else:
+        suite = unittest.TestSuite()
+        suite.addTest(VirtualFillTestCase('test_' + name))
+        unittest.TextTestRunner(verbosity=2).run(suite)
+        
+@manager.command
+def init_db():
+    """init database"""
+    db.drop_all()
+    db.create_all()
+    Role.insert_roles()
+    
+    # create adminstator
+    ad = User(email='admin@gmail.com', username='admin', password='admin', role_id=4)
+    db.session.add(ad)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
