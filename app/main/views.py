c166fd5e332d54d7eb361330bbd181cca926dc50
PrivateRookie <996514515@qq.com>
2018-01-06 23:12:46 +0800

    修改了右侧导航栏样式

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+from datetime import datetime
 from flask import render_template, redirect, url_for, flash, session, request
+from flask_login import current_user
 from . import main
 from .forms import *
-from .. import db
+from .. import db, surveymodels, models
 from ..static import raw_forms, schema
 from ..models import Permission
 from ..decorators import permission_required, admin_required
@@ -30,6 +32,11 @@ def get_pager(report_type, current_status, current_form_name):
         form_name=form_name, status=status)
     
     return pre, next
+    
+def flat(li):
+    if len(li) > 1:
+        return ''.join(str(i) for i in li)
+    return li[0]
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -46,26 +53,65 @@ def forms():
         return render_template('allforms.html')
     raw_form = getattr(raw_forms, form_name, None)
     previous, next = get_pager(report_type, status, form_name)
-    options = dict(survey=raw_form, route='main.echo', status=status, report_type=report_type,
+    options = dict(survey=raw_form, route='main.recevie', status=status, report_type=report_type,
     previous=previous, next=next)
     return render_template('rates/{}.html'.format(form_name), **options)
 
 @main.route('/selfreport')
 def selfreport():
     previous, next = get_pager('self_report', 'v0', 'cover')
-    return render_template('rates/cover.html', status='v0', route='main.echo',
+    return render_template('rates/cover.html', status='v0', route='main.recevie',
     previous=previous, next=next, report_type='self_report')
     
 @main.route('/ohterreport')
 def otherreport():
     previous, next = get_pager('other_report', 'v0', 'cover')
-    return render_template('rates/cover.html', status='v0', route='main.echo',
+    return render_template('rates/cover.html', status='v0', route='main.recevie',
     previous=previous, next=next, report_type='other_report')
     
 @main.route('/echo', methods=['GET', 'POST'])
 def echo():
-    data = [(attr, request.form.getlist(attr)) for attr in request.form.keys() if attr.startswith('q')]
+    data = {attr:flat(request.form.getlist(attr)) for attr in request.form.keys() if attr.startswith('q')}
     return render_template('echo.html', data=data)
+    
+@main.route('/recevie', methods=['GET', 'POST'])
+def recevie():
+    """
+    patient_info = session.get('patient_info', None)
+    
+    if patient_info is None:
+        patient_info = dict()
+        patient_info['name'] = request.form.get('q_2')
+        patient_info['code'] = request.form.get('q_1')
+        patient_info['writer'] = current_user.username
+        # 提交新患者信息
+        data = [flat(request.form.getlist(attr)) for attr in request.form.keys() if attr.startswith('q')]
+        data = {k:v for k, v in zip(['code', 'name', 'entry_date', 'doctor'], data)}
+        #data['entry_date'] = datetime.strptime(data['entry_date'], '%Y-%m-%d')
+        data['entry_date'] = datetime.now()
+        data['recorder_id'] = current_user.id
+        p = models.Patient(**data)
+        db.session.add(p)
+        db.session.commit()
+        patient_info['patient_id'] = p.id
+        session['patient_info'] = patient_info
+    
+    model = getattr(surveymodels, request.args['form_name'].uppercase())
+    data = {attr:flat(request.form.getlist(attr)) for attr in request.form.keys() if attr.startswith('q')}
+    data['p_id'] = patient_info['patient_id']
+    data['status'] = request.args['status']
+    record = model(**data)
+    db.session.add(record)
+    db.session.commit()
+    return render_template('echo.html', data=data)
+    """
+    data = {attr:flat(request.form.getlist(attr)) for attr in request.form.keys() if attr.startswith('q')}
+    patient_info = session.get('patient_info', None)
+    data.update(patient_info)
+    return render_template('echo.html', data=data)
+        
+        
+    
     
 @main.route('/test')
 def test():
