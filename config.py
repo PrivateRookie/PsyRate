3ff76f3e4e7b9794e9358204c200b89f56708cc2
PrivateRookie <996514515@qq.com>
2018-02-12 19:16:18 +0800

    添加生成环境设置

--- config.py
+++ config.py
@@ -4,16 +4,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-class DevelopmentConfig(Config):
-    DEBUG = True
     MAIL_PREFIX = os.environ.get('MAIL_PREFIX')
     MAIL_SENDER = os.environ.get('MAIL_SENDER')
     MAIL_SERVER = os.environ.get('SMTP_SERVER')
@@ -22,21 +15,30 @@ class DevelopmentConfig(Config):
     MIAL_USE_TLS = os.environ.get('USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    @staticmethod
+    def init_app(app):
+        pass
+        
+class ProductConfig(Config):
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SERVER_NAME=os.environ.get('SERVER_NAME')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRO_DATABASE_URL') \
+    or ('sqlite:///' + os.path.join(basedir, 'data-pro.sqlite'))
+
+class DevelopmentConfig(Config):
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') \
     or ('sqlite:///' + os.path.join(basedir, 'data-dev.sqlite'))
     
     
 class TestConfig(Config):
     DEBUG = True
-    MAIL_SERVER = os.environ.get('SMTP_SERVER')
-    MAIL_PORT = os.environ.get('SMTP_PORT')
-    MAIL_USE_SSL = os.environ.get('USE_SSL')
-    MIAL_USE_TLS = os.environ.get('USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') \
     or ('sqlite:///' + os.path.join(basedir, 'data-test.sqlite'))
 
-config = {'default':DevelopmentConfig,
-          'dev':DevelopmentConfig,
-          'test':TestConfig}
+config = {'DEFAULT':DevelopmentConfig,
+          'DEV':DevelopmentConfig,
+          'PRODUCT':ProductConfig,
+          'TEST':TestConfig}
